bbb8ee2e09b0597c9bd06f1a8b362b2253694f67
Nyandori <iomullongare@gmail.com>
2022-06-18 20:19:23 +0300

    Final for Today

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
 
-<?php
-$host = 'nyandori.mysql.database.azure.com';
-$username = 'Jakom';
-$password = '0746282760@Ngare';
-$db_name = 'test';
-
-//Establishes the connection
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, ssl-mode=require);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-echo "Connected Good";
-// Run the create table query
-if (mysqli_query($conn, '
-CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
-);
-')) {
-printf("Table created\n");
-}
-
-//Close the connection
-mysqli_close($conn);
-?>
-
 <html>
 <head> 
 <style>
